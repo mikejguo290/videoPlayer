da1c68a5dcf1fb6739666bd52def914ddadc43f3
mikejguo290 <mikejguo290@gmail.com>
2021-05-24 13:28:16 +0100

    8. Bind this.handleClick to the current value of this and store it in this.handleClick within constructor.

--- src/Menu.js
+++ src/Menu.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 export class Menu extends React.Component {
-  
+  constructor(props){
+    super(props);
+    this.handleClick=this.handleClick.bind(this);
+  }
+
   handleClick(e){
     // wrap the set state, chooseVideo inside a wrapper which processes the event input. get the event handler on form to call this. 
     const text = e.target.value;
